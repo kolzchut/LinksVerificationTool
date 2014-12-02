f3db0429bb58c1dc65117984a89b4e06d97e28dc
giledri <giledri@gmail.com>
2014-12-02 22:03:32 +0200

    Add support for IDN (hebrew) URLs
    translate IDN URLs to ascii so it can be resolved

---
+++ KolZchutLinksVerification/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KolZchutLinksVerification
+{
+	public static class StringExtension
+	{
+		public static bool IsNullOrWhiteSpace(this String s)
+		{
+			return (s == null || String.IsNullOrEmpty(s.TrimStart()));
+		}
+	}
+}
