0f0b9c16594736291809702bc78536d979324eed
Dror S <FreedomFighterSparrow@kolzchut.org.il>
2014-12-29 17:43:15 +0200

    Improve sql query for external links

--- externallinks.sql
+++ externallinks.sql
@@ -1,4 +1,37 @@
-SELECT DISTINCT `el_to` FROM `externallinks` 
-;and then export everything, check using Xenu
-;maybe!: select el_from for every broken link later.
-;alternatively: make volunteer use special:חיפוש קישורים יוצאים
+SELECT page.page_namespace, page.page_title, externallinks.el_to FROM `externallinks` INNER JOIN page ON page.page_id = externallinks.el_from
+
+
+-- Version that automatically adds the namespace to the title:
+SELECT 
+	CONCAT(
+		CASE page_namespace
+			-- MediaWiki standard (canonical) namespaces
+			WHEN 0 THEN ''
+			WHEN 1 THEN 'Talk:'
+			WHEN 2 THEN 'User:'
+			WHEN 4 THEN 'Project:'
+			WHEN 6 THEN 'File:'
+			WHEN 8 THEN 'Mediawiki:'
+			WHEN 10 THEN 'Template:'
+			WHEN 12 THEN 'Help:'
+			WHEN 14 THEN 'Category:'
+
+			-- MediaWiki standard extension
+			WHEN 274 THEN 'Widget:'
+
+			-- Kol-Zchut cusom namespaces:
+			WHEN 110 THEN 'אודות:'
+			WHEN 112 THEN 'קהילת_ידע:'
+			WHEN 116 THEN 'חדש:'
+			WHEN 118 THEN 'הקפאה:'
+			WHEN 120 THEN 'תרגול:'
+			WHEN 122 THEN 'נתון:'
+			
+			-- Default so we can see if the above isn't comprehensive enough:
+			ELSE CONCAT(page_namespace,':')
+		END,
+	page_title) as title,
+	externallinks.el_to
+FROM `externallinks`
+INNER JOIN page ON page.page_id = externallinks.el_from
+
